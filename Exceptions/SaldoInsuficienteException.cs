63d57b0174386782fbf0522b81d119219c82d89c
Gabriel <gabrielgbr.contato@hotmail.com>
2022-09-12 19:12:51 -0300

    Criação de classe de exceção para saldo insuficiente

--- Exceptions/SaldoInsuficienteException.cs
+++ Exceptions/SaldoInsuficienteException.cs
@@ -7,8 +7,6 @@ namespace PoupaDev.API.Exceptions
 {
     public class SaldoInsuficienteException : Exception
     {
-        public SaldoInsuficienteException() : base("Saldo insuficiente!")
-        {
-        }
+        public SaldoInsuficienteException() : base("Saldo insuficiente!") { }
     }
 }
