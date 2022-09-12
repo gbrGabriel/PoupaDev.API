dfe7c6344981780ab5a61a9860051aa7ae329afc
Gabriel <gabrielgbr.contato@hotmail.com>
2022-09-12 19:11:59 -0300

    Alterações na entidade

--- Entities/Operacao.cs
+++ Entities/Operacao.cs
@@ -8,18 +8,17 @@ namespace PoupaDev.API.Entities
 {
     public class Operacao
     {
-        public Operacao(decimal valor, TipoOperacao tipo)
+        public Operacao(decimal valor, TipoOperacao tipo, int idObjetivo)
         {
-            Id = new Random().Next(1, 1000);
             Valor = valor;
             Tipo = tipo;
-
+            IdObjetivo = idObjetivo;
             DataOperacao = DateTime.Now;
         }
-
         public int Id { get; private set; }
         public decimal Valor { get; private set; }
         public TipoOperacao Tipo { get; private set; }
         public DateTime DataOperacao { get; set; }
+        public int IdObjetivo { get; set; }
     }
 }
